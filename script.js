5c21bc026d9c04ff5fd3725aebcfd81a666713f2
Devi Jagannadh Kotha <72683351+DeviJagannadh-TechDevp@users.noreply.github.com>
2023-12-25 11:26:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Add your existing JavaScript code here
+
+// Testimonial Carousel Initialization
+document.addEventListener('DOMContentLoaded', function () {
+    var testimonialCarousel = new Glide('.testimonial-carousel', {
+        type: 'carousel',
+        perView: 1,
+        focusAt: 'center',
+        gap: 20, // Adjust the gap as needed
+    });
+
+    testimonialCarousel.mount();
+
+    // Navigation Button Event Listeners
+    document.querySelector('.prev-btn').addEventListener('click', function () {
+        testimonialCarousel.go('<');
+    });
+
+    document.querySelector('.next-btn').addEventListener('click', function () {
+        testimonialCarousel.go('>');
+    });
+});
+
+function enlargeImage() {
+    // Get the original and enlarged image elements
+    var originalImage = document.getElementById('oil-painting-image');
+    var enlargedImage = document.getElementById('enlarged-oil-painting');
+
+    // Set the source of the enlarged image to be the same as the original image
+    enlargedImage.src = originalImage.src;
+
+    // Display the enlarged image container
+    document.getElementById('enlarged-image').style.display = 'block';
+}
